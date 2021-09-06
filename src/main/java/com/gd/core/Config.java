797023364de33f0450de8a9a40bd603f18baf781
pylie <pylie@gmail.com>
2021-09-06 10:48:09 +0430

    add Config.java

---
+++ src/main/java/com/gd/core/Config.java
@@ -0,0 +1,31 @@
+package com.gd.core;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class Config {
+
+    private final MessageSource messageSource;
+
+    public Config(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Bean
+    public MessageSourceAccessor messageSourceAccessor(MessageSource messageSource) {
+        return new MessageSourceAccessor(messageSource);
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator(){
+        LocalValidatorFactoryBean validatorFactoryBean=new LocalValidatorFactoryBean();
+        validatorFactoryBean.setValidationMessageSource(messageSource);
+        return validatorFactoryBean;
+    }
+
+
+}
